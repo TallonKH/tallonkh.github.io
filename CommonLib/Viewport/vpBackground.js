5d691e1ae9da037d4065ccc002eaac42276f7940
Tallon Hodge <tallon@cs.uchicago.edu>
2020-01-18 14:54:52 -0600

    various usability improvements

---
+++ CommonLib/Viewport/vpBackground.js
@@ -0,0 +1,41 @@
+class VPBackground extends VPObject {
+    constructor(viewport, {} = {}) {
+        super(viewport, {
+            "mouseListening": true,
+            "zOrder": -65536
+        })
+        this.color = "#ebebeb";
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(0, 0, this.vp.canvas.width, this.vp.canvas.height);
+    }
+
+    isOverlapping(point) {
+        return true;
+    }
+
+    isMouseBlockingOverlap() {
+        return true;
+    }
+
+    isMouseBlockingPress() {
+        return true;
+    }
+
+    onDragStarted() {
+        super.onDragStarted();
+        this.suggestCursor("move");
+    }
+    
+    onDragged() {
+        super.onDragged();
+        this.vp.panCenter = this.vp.panCenter.addp(this.vp.mouseElemDelta);
+    }
+
+    onDragEnded() {
+        super.onDragEnded();
+        this.unsuggestCursor("move");
+    }
+}
